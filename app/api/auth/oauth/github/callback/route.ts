9ce262166ee535649f62b3ef38afadbf09763e2a
Rachit Bhatta <rachitbhatta2009@gmail.com>
2025-12-15 16:06:58 +0545

    debugged github anf google callback route

--- app/api/auth/oauth/github/callback/route.ts
+++ app/api/auth/oauth/github/callback/route.ts
@@ -140,7 +140,7 @@ export async function GET(req: NextRequest) {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: "lax",
-            maxAge: 7 * 24 * 60 * 60,
+            maxAge: 60 * 60,
             path: "/"
         });
 
